b331f5d38cb18257318bba0736c8b3923a54cf9f
Raunak445 <raunakkumar445@gmail.com>
2024-02-16 12:27:59 +0530

    SocketProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 // import './index.css'
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
-import { SocketProvider } from "./context/SocketProvider";
+import { SocketProvider } from './context/socketProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <SocketProvider>
